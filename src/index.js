85bea6f5eb2dcce5689ba2a5be636787576c5d35
lucasmp <lucasmatiasperez@gmail.com>
2022-02-03 17:53:19 -0300

    Agrega logica principal para el funcionamiento de la app

---
+++ src/index.js
@@ -0,0 +1,73 @@
+const API_KEY = '67576d3b7a1414279df98d73';
+const BASE_URL = 'https://v6.exchangerate-api.com/v6/';
+
+const $baseTabla = document.querySelector('#base-tabla');
+const $generarTabla = document.querySelector('#generar-tabla');
+const $tabla = document.querySelector('#tabla');
+
+async function cargarMonedas() {
+    try {
+        const response = await fetch(`${BASE_URL + API_KEY}/codes`);
+        const data = await response.json();
+        return data.supported_codes;
+    } catch (error) {
+        return console.error("Error:", error);
+    }
+}
+
+function listarMonedas() {
+    cargarMonedas().then(monedas => {
+        monedas.forEach((moneda) => {
+            const $option = document.createElement('option');
+            $option.setAttribute('value', moneda[0]);
+            $option.textContent = `${moneda[0]} (${moneda[1]})`;
+            $baseTabla.appendChild($option);
+        });
+    });
+}
+
+function manejarClickGenerarTabla() {
+    $generarTabla.onclick = function (e) {
+        const base = $baseTabla.options[$baseTabla.selectedIndex].value;
+        cargarCambios(base).then(data => {
+            crearElementosDeTabla(data);
+            $tabla.classList.remove('invisible');
+        });    
+        e.preventDefault();
+    };
+}
+
+async function cargarCambios(base) {
+    try {
+        const response = await fetch(`${BASE_URL + API_KEY}/latest/${base}`);
+        return await response.json();
+    } catch (error) {
+        return console.error("Error:", error);
+    }
+}
+
+function crearElementosDeTabla(data) {
+    const $caption = $tabla.querySelector('caption');
+    const $bodyTabla = $tabla.querySelector('tbody');
+
+    $bodyTabla.innerHTML = "";
+    $caption.textContent = `Base: ${data.base_code} - Fecha: ${data.time_last_update_utc.split(' ').slice(0, 4).join(' ')}`;
+
+    Object.entries(data.conversion_rates).forEach((parModedaValor) => {
+        const $fila = document.createElement('tr');
+        const $celda1 = document.createElement('td');
+        $celda1.textContent = parModedaValor[0];
+        const $celda2 = document.createElement('td');
+        $celda2.textContent = parModedaValor[1];
+        $fila.appendChild($celda1);
+        $fila.appendChild($celda2);
+        $bodyTabla.appendChild($fila);
+    });
+}
+
+function iniciar() {
+    listarMonedas();
+    manejarClickGenerarTabla();
+}
+
+iniciar();
